e527e2724cfd39cd7c6e231f87ca9db66b8cfa11
Sergio Rojas <sergio.rojas.cch@gmail.com>
2021-11-03 14:27:52 -0700

    CardsController

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,8 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Activity;
+use App\Models\User;
+use App\Models\Information;
+use App\Models\Card;
 
 class CardController extends Controller
 {
-    //
+    public function index()
+    {    
+        $id = Auth::id();
+        $cards = Card::where('id_usuario', $id);
+
+        return view('myCards', compact('cards'));
+    }
+
+    public function store(Request $request)
+    {
+        $card = Card::create($request->all());
+
+        return redirect()->back();
+    }
+
+    public function update(Request $request)
+    {
+        $card = Card::find($request->id);
+
+        if($card)
+        {
+            if($card->update($request->all()))
+            {
+                return redirect()->back();
+            }
+        }
+
+        return redirect()->back();
+    }
+    
+    public function getCardsByTheme($id)
+    {
+        $cards = Card::where('id_information', $id);
+      
+        if(count($cards) == 0)
+        {
+            return response()->json([
+                'message' => 'Sin resultados.',
+            ], 200);
+        }
+        else
+        {
+            return response()->json([
+                'message' => 'Busqueda Exitosa',
+                'articulos' => $cards,
+            ], 200);
+        }
+    }
+
+    public function destroy($id)
+    {
+        Card::destroy($id);
+      
+        return view('myCards');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*MENU ROUTES*/
 Route::get('/', 'ActivityController@index')->name('index');
 
 Route::get('estudioMenu', 'ActivityController@estudioMenu')->name('estudioMenu');
 Route::get('juegosMenu', 'ActivityController@juegosMenu')->name('juegosMenu');
 
+/*GAME&SCORE ROUTES*/
 Route::get('invaders', 'GameController@invaders')->name('invaders');
 Route::get('memorama', 'GameController@memorama')->name('memorama');
 Route::get('quiz', 'GameController@quizz')->name('quizz');
 route::post('score', 'GameController@score')->name('inject.score');
-
-Route::get('estudio/{id}', 'ActivityController@estudio')->name('estudio');
-
 Route::get('scoreTable', 'GameController@scoreTable')->name('score.table');
 
+/*Information ROUTES*/
+Route::get('estudio/{id}', 'ActivityController@estudio')->name('estudio');
 Route::get('tutorial/{id}', 'ActivityController@tutorial')->name('tutorial');
 
+/*CARDS ROUTES*/
+Route::middleware(['auth'])->group(function() {
+
+	Route::get('misCartas', 'CardController@index')->name('myCards');
+	Route::get('getCardsByTheme/{id}', 'CardController@getCardsByTheme')->name('cardsByTheme');
+	Route::post('storeCard', 'CardController@store')->name('myCards_store');
+	Route::put('updateCard', 'CardController@update')->name('myCards_update');
+	Route::delete('deleteCard', 'CardController@destroy')->name('myCards_delete');
+	
+});
+
+/*DASHBOARD*/
 Route::get('dashboard', 'GameController@dashboard')->name('dashboard');
 
+/*USER CRUD & LOGIN*/
 Auth::routes();
 
+
+/*TEST*/
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
