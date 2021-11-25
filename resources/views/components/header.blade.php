26d8f38a5435839fc914de4b736bf0baf6cc5902
Sergio Rojas <sergio.rojas.cch@gmail.com>
2021-11-25 11:27:05 -0700

    Fixed button

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-</div>
+<button class="w-12 h-12 m-4 rounded-full left-0 bottom-0 absolute animate-bounce" onclick="oyecy()" style="background-color:#548B2E;">
+    <img class="mx-auto" src="Icons/music-svgrepo-com.svg" style="height: 100%; width: 100%; color: white;">
+</button>
+
+<script type="text/javascript">
+   function oyecy()
+   {
+        console.log("puto sandoval");
+   }
+</script>
