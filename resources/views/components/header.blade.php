4da274a55af3cd16321af0b63d1d6dbc149b8c8c
Sergio Rojas <sergio.rojas.cch@gmail.com>
2021-11-25 11:44:46 -0700

    Test

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<button class="w-12 h-12 m-4 rounded-full left-0 bottom-0 absolute animate-bounce" onclick="oyecy()" style="background-color:#548B2E;">
+<button id="modal" class="w-12 h-12 m-4 rounded-full left-0 bottom-0 absolute animate-bounce" onclick="oyecy()" style="background-color:#548B2E;">
     <img class="mx-auto" src="Icons/music-svgrepo-com.svg" style="height: 100%; width: 100%; color: white;">
 </button>
 
